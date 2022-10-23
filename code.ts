ed944098b569ed459c341be503f2deb524c4cc83
Ali Paredes <alinorberto168@gmail.com>
2022-10-23 18:23:40 -0400

    v1 form plugin

--- code.ts
+++ code.ts
@@ -1,87 +1,89 @@
-// This file holds the main code for the plugin. It has access to the *document*.
-// You can access browser APIs such as the network by creating a UI which contains
-// a full browser environment (see documentation).
+figma.showUI(__html__);
 
-// Runs this code if the plugin is run in Figma
-if (figma.editorType === 'figma') {
-  // This plugin will open a window to prompt the user to enter a number, and
-  // it will then create that many rectangles on the screen.
+figma.ui.resize(500, 500);
 
-  // This shows the HTML page in "ui.html".
-  figma.showUI(__html__);
+figma.ui.onmessage = async (pluginMessage) => {
+  await figma.loadFontAsync({ family: "Rubik", style: "Regular" });
 
-  // Calls to "parent.postMessage" from within the HTML page will trigger this
-  // callback. The callback will be passed the "pluginMessage" property of the
-  // posted message.
-  figma.ui.onmessage = msg => {
-    // One way of distinguishing between different types of messages sent from
-    // your HTML page is to use an object with a "type" property like this.
-    if (msg.type === 'create-shapes') {
-      const nodes: SceneNode[] = [];
-      for (let i = 0; i < msg.count; i++) {
-        const rect = figma.createRectangle();
-        rect.x = i * 150;
-        rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
-        figma.currentPage.appendChild(rect);
-        nodes.push(rect);
-      }
-      figma.currentPage.selection = nodes;
-      figma.viewport.scrollAndZoomIntoView(nodes);
+  const nodes: SceneNode[] = [];
+
+  const postComponentSet = figma.root.findOne(
+    (node) => node.type === "COMPONENT_SET" && node.name === "post"
+  ) as ComponentSetNode;
+
+  let selectedVariant;
+
+  if (pluginMessage.darkModeState === true) {
+    switch (pluginMessage.imageVariant) {
+      case "2":
+        selectedVariant = postComponentSet.findOne(
+          (node) =>
+            node.type === "COMPONENT" &&
+            node.name === "Image=single, Dark mode=true"
+        ) as ComponentNode;
+        break;
+      case "3":
+        selectedVariant = postComponentSet.findOne(
+          (node) =>
+            node.type === "COMPONENT" &&
+            node.name === "Image=carousel, Dark mode=true"
+        ) as ComponentNode;
+        break;
+      default:
+        selectedVariant = postComponentSet.findOne(
+          (node) =>
+            node.type === "COMPONENT" &&
+            node.name === "Image=none, Dark mode=true"
+        ) as ComponentNode;
     }
-
-    // Make sure to close the plugin when you're done. Otherwise the plugin will
-    // keep running, which shows the cancel button at the bottom of the screen.
-    figma.closePlugin();
-  };
-
-// If the plugins isn't run in Figma, run this code
-} else {
-  // This plugin will open a window to prompt the user to enter a number, and
-  // it will then create that many shapes and connectors on the screen.
-
-  // This shows the HTML page in "ui.html".
-  figma.showUI(__html__);
-
-  // Calls to "parent.postMessage" from within the HTML page will trigger this
-  // callback. The callback will be passed the "pluginMessage" property of the
-  // posted message.
-  figma.ui.onmessage = msg => {
-    // One way of distinguishing between different types of messages sent from
-    // your HTML page is to use an object with a "type" property like this.
-    if (msg.type === 'create-shapes') {
-      const numberOfShapes = msg.count;
-      const nodes: SceneNode[] = [];
-      for (let i = 0; i < numberOfShapes; i++) {
-        const shape = figma.createShapeWithText();
-        // You can set shapeType to one of: 'SQUARE' | 'ELLIPSE' | 'ROUNDED_RECTANGLE' | 'DIAMOND' | 'TRIANGLE_UP' | 'TRIANGLE_DOWN' | 'PARALLELOGRAM_RIGHT' | 'PARALLELOGRAM_LEFT'
-        shape.shapeType = 'ROUNDED_RECTANGLE'
-        shape.x = i * (shape.width + 200);
-        shape.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
-        figma.currentPage.appendChild(shape);
-        nodes.push(shape);
-      };
-
-      for (let i = 0; i < (numberOfShapes - 1); i++) {
-        const connector = figma.createConnector();
-        connector.strokeWeight = 8
-
-        connector.connectorStart = {
-          endpointNodeId: nodes[i].id,
-          magnet: 'AUTO',
-        };
-
-        connector.connectorEnd = {
-          endpointNodeId: nodes[i+1].id,
-          magnet: 'AUTO',
-        };
-      };
-
-      figma.currentPage.selection = nodes;
-      figma.viewport.scrollAndZoomIntoView(nodes);
+  } else {
+    switch (pluginMessage.imageVariant) {
+      case "2":
+        selectedVariant = postComponentSet.findOne(
+          (node) =>
+            node.type === "COMPONENT" &&
+            node.name === "Image=single, Dark mode=false"
+        ) as ComponentNode;
+        break;
+      case "3":
+        selectedVariant = postComponentSet.findOne(
+          (node) =>
+            node.type === "COMPONENT" &&
+            node.name === "Image=carousel, Dark mode=false"
+        ) as ComponentNode;
+        break;
+      default:
+        selectedVariant = postComponentSet.defaultVariant as ComponentNode;
     }
+  }
 
-    // Make sure to close the plugin when you're done. Otherwise the plugin will
-    // keep running, which shows the cancel button at the bottom of the screen.
-    figma.closePlugin();
-  };
+  const newPost = selectedVariant.createInstance();
+
+  const templateName = newPost.findOne(
+    (node) => node.name === "displayName" && node.type === "TEXT"
+  ) as TextNode;
+  const templateUsername = newPost.findOne(
+    (node) => node.name === "@username" && node.type === "TEXT"
+  ) as TextNode;
+  const templateDescription = newPost.findOne(
+    (node) => node.name === "description" && node.type === "TEXT"
+  ) as TextNode;
+  const numLikes = newPost.findOne(
+    (node) => node.name === "likesLabel" && node.type === "TEXT"
+  ) as TextNode;
+  const numComments = newPost.findOne(
+    (node) => node.name === "commentsLabel" && node.type === "TEXT"
+  ) as TextNode;
+
+  templateName.characters = pluginMessage.name;
+  templateUsername.characters = pluginMessage.username;
+  templateDescription.characters = pluginMessage.description;
+  numLikes.characters = Math.floor(Math.random() * 1000 + 1).toString();
+  numComments.characters = Math.floor(Math.random() * 1000 + 1).toString();
+
+  nodes.push(newPost);
+
+  figma.viewport.scrollAndZoomIntoView(nodes);
+
+  figma.closePlugin();
 };
